f3c16d07bd6f7b887f0e66cafd9d4e92d9f051b8
Md-Sifat-code <codewithsifat4@gmail.com>
2024-10-30 00:28:28 +0600

    adding the day-02 exercise-01

---
+++ Day-02/Exercise/Ex-01.py
@@ -0,0 +1,10 @@
+x = input("What is your provided number: ")
+input_1 = int(x[0])
+input_2 = int(x[1])
+
+sum = input_1+input_2
+print(sum)
+# Let's explain what i did here
+# First taking input from the user then taking the 1st and last element like using string split
+# Then converting them into int values
+# Last we added them together and got an answer
