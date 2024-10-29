dd3e7934bc590e258807385f5178792cf52a7d97
Md-Sifat-code <codewithsifat4@gmail.com>
2024-10-29 19:32:24 +0600

    Debug exercise addiing-2

--- Day-01/Exercise/Debug.py
+++ Day-01/Exercise/Debug.py
@@ -3,3 +3,8 @@ print("Hello" +" "+"Sifat")
 # 1. Giving An additional Space Before The print
 # 2. Mistake in Giving "" marks before or after the code
 #3. Mistake in giving the + to combine two Strings
+
+print("Day 1 - String Manipulation")
+print("String Concatenation is done with the "+" sign.")
+print('e.g. print("Hello " + "world")')
+print(("New lines can be created with a backslash and n."))
