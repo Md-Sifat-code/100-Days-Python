9f86cbb54d70ed85a760817a133f844bf6c89419
Md-Sifat-code <codewithsifat4@gmail.com>
2024-10-29 20:20:50 +0600

    Day-01 Ended with this project

---
+++ Day-01/Day-01-Project.py
@@ -0,0 +1,4 @@
+x = input("What is the name of your city you born in? \n")
+y = input("What is the pet name that you adopted first? \n ")
+print("Your Band name should be "+x+" "+y)
+# with this project the Day 01 is ended
---
+++ Day-01/Variables.py
@@ -0,0 +1,5 @@
+name ="x"
+name_of_sifat = "Sifat"
+# you can use any upper case letter at the bigining of the variables
+# we can not use any sign or numbers at the start of a variable nam
+# We can not use Camel case part in here also
