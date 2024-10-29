796bb0f289680af1a0dce418a4564b204dd0ceb9
Md-Sifat-code <codewithsifat4@gmail.com>
2024-10-29 19:30:14 +0600

    Debug exercise addiing

---
+++ Day-01/Exercise/Debug.py
@@ -0,0 +1,5 @@
+print("Hello" +" "+"Sifat")
+# In here we can get 3 Typer of bug
+# 1. Giving An additional Space Before The print
+# 2. Mistake in Giving "" marks before or after the code
+#3. Mistake in giving the + to combine two Strings
