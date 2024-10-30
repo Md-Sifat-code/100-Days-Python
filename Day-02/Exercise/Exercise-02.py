ec39d3b9dfd8e2c34846f097620c7d757e178d84
Md-Sifat-code <codewithsifat4@gmail.com>
2024-10-30 18:36:46 +0600

    Day-02 F String Concept

---
+++ Day-02/Exercise/Exercise-02.py
@@ -0,0 +1,5 @@
+x= int(input("Enter your height in m: \n"))
+y = int(input("Enter your weight in kg: \n"))
+
+BMI = y/(x*x)
+print(BMI)
---
+++ Day-02/F String .py
@@ -0,0 +1,8 @@
+score = 0
+height = 12.4
+isWining = True
+
+print(f"Your Score is {score}, your height is {height}, And congratulation you are wining {isWining}")
+
+# This is the way we can add various types of values in one string
+# Using an f before the "" we can then dynamically add the values of other types data
