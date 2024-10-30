3364b86bdee84e23e07db03d1a0f99eff2dc3986
Md-Sifat-code <codewithsifat4@gmail.com>
2024-10-30 22:31:08 +0600

    exercise-01 added even or odd number

---
+++ Day-03/Exercise/Odd or even.py
@@ -0,0 +1,10 @@
+x = int(input("Enter a number: \n"))
+if x%2 == 0:
+    print(f"{x} is an Even number")
+else:
+    print(f"{x} is a Odd number")
+
+
+
+# This exercise is helping user to know his given number is even or odd.
+# we are running a simple logic if/else operation to define the user given number is even or odd
