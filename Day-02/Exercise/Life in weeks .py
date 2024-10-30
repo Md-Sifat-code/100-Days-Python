90e9a0b437ee3427bde8ae0ef1fcae4c6c918ba3
Md-Sifat-code <codewithsifat4@gmail.com>
2024-10-30 19:00:21 +0600

    Life in weeks exercise

---
+++ Day-02/Exercise/Life in weeks .py
@@ -0,0 +1,7 @@
+age = int(input("Enter your age in years: "))
+years_remaining = 90-age
+days_remaining = years_remaining*365
+weeks_remaining = years_remaining*52
+months_remaining = years_remaining*12
+
+print(f"You have still {years_remaining} years,{days_remaining} days,{weeks_remaining} weeks, {months_remaining} months remaining to get 98 years old")
