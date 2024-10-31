29a88d456c05a91e1c82332a6f21baf0ef71f0d0
Md-Sifat-code <codewithsifat4@gmail.com>
2024-10-31 23:06:08 +0600

    Added the nested list part

---
+++ Day-04/Nested_list.py
@@ -0,0 +1,5 @@
+x = ["Fruits", "Apple", "Banana", "Cherry"]
+y=["Vegetables", "Carrot", "Broccoli", "Spinach"]
+z = ["Grains", "Rice", "Wheat", "Barley"]
+text_data = [x,y,z]
+# Here added list into a whole big list as it is a nested list stored together
