139b88751f333e6dac05d990a033302cfc3d7399
Md-Sifat-code <codewithsifat4@gmail.com>
2024-11-03 02:33:51 +0600

    adding the extra exercise -02

---
+++ Day-05/Exercise/extra-ex-02.py
@@ -0,0 +1,7 @@
+n = int(input("Enter How many times you want to run the loop? \n"))
+sum =0
+for x in range(n):
+    sum+=x
+
+print(sum)
+# This exercise to get the sum of the natural numbers given by the user
