8edc56484f37da50366989d59456126d3cdf069d
Md-Sifat-code <codewithsifat4@gmail.com>
2024-11-03 02:27:30 +0600

    adding the exercise-02

---
+++ Day-05/Exercise/High Score.py
@@ -0,0 +1,6 @@
+students_score = [78,65,89,86,55,91,64,89]
+
+high_score = max(students_score)
+
+print(f"The Height score is {high_score}")
+# In this coding exercise just used the max function to get the high score between some vales
