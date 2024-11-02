be94722c33a9b6f551a34b3a691d8d6ad7049d06
Md-Sifat-code <codewithsifat4@gmail.com>
2024-11-03 01:43:46 +0600

    Day-05 Begin here

---
+++ Day-05/for loop.py
@@ -0,0 +1,10 @@
+fruits = ["apple", "banana", "cherry"]
+for fruit in fruits:
+    print(fruit)
+# This is the new way of writing the for loop. It's kind of like for each loops in other programming languages
+
+
+fruits = ["apple", "banana", "cherry"]
+for i in range(len(fruits)):
+    print(fruits[i])
+# This one is the traditional way of writing the for loop where we use range . Both of them works well and perfectly
