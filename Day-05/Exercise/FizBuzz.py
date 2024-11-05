cb213eb2c838cc9d3c611e7c7b9eb140fe469fc7
Md-Sifat-code <codewithsifat4@gmail.com>
2024-11-06 00:56:35 +0600

    adding the FizzBuzz exercise

---
+++ Day-05/Exercise/FizBuzz.py
@@ -0,0 +1,12 @@
+for x in range(1,100):
+    if x%3==0:
+        print("Fizz")
+    elif x%5==0:
+        print("Buzz")
+    else:
+        print(x)
+
+#  This code will run a for loop range type . in the loop the range is mentioned like (0,100) here the 0 means the number ranger will start from here
+# it will end on 100 . The main logic of this code is to check conditional operation in the for loop
+#  It will print Fizz if the number is perfectly devied by the 3 and Buzz if the number devided by 5.
+#  Others it will just print the number stored into the x
