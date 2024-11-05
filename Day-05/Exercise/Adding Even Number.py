064c8cdd3461c72176bba60111704e4bb7eeba5d
Md-Sifat-code <codewithsifat4@gmail.com>
2024-11-06 00:32:53 +0600

    adding the Even Number sum exercise

---
+++ Day-05/Exercise/Adding Even Number.py
@@ -0,0 +1,10 @@
+total = 0
+
+for x in range(100):
+    if x%2==0:
+        total+=x
+
+print(f"The Total of Even numbers is: {total}")
+
+
+# This code is to calculate the total value of the even numbers from 0 to 100 using the for loop in range type
