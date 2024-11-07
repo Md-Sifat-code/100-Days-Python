1c9aa2302e810c8f7249aed29fe58d4a80a356ef
Md-Sifat-code <codewithsifat4@gmail.com>
2024-11-07 10:11:36 +0600

    adding the Hurdles Loop challange

---
+++ Day-06/The Hurdles Loop Challange.py
@@ -0,0 +1,20 @@
+def turnLeft ():
+
+    print("turn left")
+def turnRight ():
+    print("turn left")
+    print("turn left")
+    print("turn left")
+def looping ():
+    print("move")
+    turnLeft()
+    print("move")
+    turnRight()
+    print("move")
+    turnRight()
+    print("move")
+    turnLeft()
+for x in range(6):
+    looping()
+    print(f"End {x}")
+
