1d9a5374ec43058368c611a353bb4ce3c1070eba
Md-Sifat-code <codewithsifat4@gmail.com>
2024-11-09 21:05:43 +0600

    Day-08 started

---
+++ Day-08/Function with Inputs.py
@@ -0,0 +1,9 @@
+def greet_with_name(name):
+    print(f"Hello there {name}")
+    print(f"How are you today {name}")
+
+
+greet_with_name("Sifat")
+
+
+# In here we are using an call back function with perameters. which will be send to the function when user will send a value
