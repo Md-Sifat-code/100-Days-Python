f178a76b2add6ffe502f523a8d2f023e56720500
Md-Sifat-code <codewithsifat4@gmail.com>
2024-11-11 21:32:02 +0600

    updating the ceaser cipher

--- Day-08/Caear Cipher part-1-Encryption.py
+++ Day-08/Caear Cipher part-1-Encryption.py
@@ -1,29 +1,50 @@
+from art import logo
+print(logo)
+
+
 alphabet =['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
     ,'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number :\n"))
+
 
 def encrypt(plain_text,number):
     cipher_text =""
     for letter in plain_text:
-        position = alphabet.index(letter)
-        new_position = position + number
-        new_letter = alphabet[new_position]
-        cipher_text += new_letter
+        if letter in alphabet:
+            position = alphabet.index(letter)
+            new_position = position + number
+            new_letter = alphabet[new_position]
+            cipher_text += new_letter
+        else:
+            cipher_text+=letter
     print(f"The encoded text is {cipher_text}")
 
 
 def decrypt(plain_text,number):
     cipher_text =""
     for letter in plain_text:
-        position = alphabet.index(letter)
-        new_position = position - number
-        new_letter = alphabet[new_position]
-        cipher_text += new_letter
+        if letter in alphabet:
+            position = alphabet.index(letter)
+            new_position = position - number
+            new_letter = alphabet[new_position]
+            cipher_text += new_letter
+        else:
+            cipher_text += letter
     print(f"The decoded text is {cipher_text}")
 
 
-decrypt(text,shift)
-encrypt(text,shift)
+
+while True:
+    want = input("You want to play?\n")
+    if want !="yes":
+        print("Good Bye")
+    else:
+        text = input("Type your message:\n").lower()
+        shift = int(input("Type the shift number :\n"))
+        shift = shift % 26
+        to_do = input("Which typer you want to do?\n")
+        if to_do == "decode":
+            decrypt(text, shift)
+        elif to_do == "encode":
+            encrypt(text, shift)
---
+++ Day-08/art.py
@@ -0,0 +1,16 @@
+logo = """
+           ,adPPYba,  ,adPPYba,  ,adPPYba,  ,adPPYba,   8b,dPPYba,
+           ""     `Y8  a8P_____88  ""     `Y8  ""     `Y8   88P'   "Y8
+           ,adPPPPP88  8PP"""""""  ,adPPPPP88  ,adPPPPP88   88
+           88,    ,88  "8b,   ,aa  88,    ,88  88,    ,88   88
+           `"8bbdP"Y8   `"8bbdP"Y8 `"8bbdP"Y8  `"8bbdP"Y8   88
+
+
+           ,adPPYba,  88 8b,dPPYba,   ,adPPYba,  ,adPPYba,   8b,dPPYba,
+           I8[    ""  88 88P'    "8a  I8[    ""  I8[    ""   88P'   "Y8
+           `"Y8ba,    88 88       d8   `"Y8ba,    `"Y8ba,    88
+           aa    ]8I  88 88b,   ,a8"   aa    ]8I  aa    ]8I  88
+           `"YbbdP"'  88 88`YbbdP"'    `"YbbdP"'  `"YbbdP"'  88
+                       88 88
+
+"""
