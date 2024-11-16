fdfce7ed005f2d6b51b032ce89e0c0b11d6a6f75
Md-Sifat-code <codewithsifat4@gmail.com>
2024-11-16 23:41:43 +0600

    Starting of Day-10

---
+++ Day-10/Fucntions with outputs.py
@@ -0,0 +1,6 @@
+def formate_name(f_name,l_name):
+    formated_f_name = f_name.title()
+    formated_l_name = l_name.title()
+    return f"{formated_f_name} {formated_l_name}"
+
+print(formate_name("Sifat", "js"))
